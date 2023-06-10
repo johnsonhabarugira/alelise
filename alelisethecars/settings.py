589562676635059c34e0a70e71d28dd2e1f3ab29
HABARUGIRA Johnson <habarugirajohnson@gmail.com>
2023-06-10 12:38:18 +0200

    Update settings.py

--- alelisethecars/settings.py
+++ alelisethecars/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.alelisethecars.com', 'alelisethecars.com','127.0.0.1','alelisethecars.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 
