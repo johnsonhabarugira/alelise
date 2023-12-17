933a127dfce035f5ef94ee16dea70c2b8467e131
Johnson HABARUGIRA <habarugirajohnson@gmail.com>
2023-12-17 16:12:42 +0200

    removed pagination

--- alelisethecars/settings.py
+++ alelisethecars/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.alelisethecars.com', 'alelisethecars.com','127.0.0.1','alelisethecars.onrender.com']
 
